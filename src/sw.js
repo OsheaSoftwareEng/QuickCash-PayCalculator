b23115d3d59d23a6193a4f91914ebd6a0e6c16a8
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-08-22 12:27:31 -0400

    updated and converted app to pwa

---
+++ src/sw.js
@@ -0,0 +1,15 @@
+// const staticQuickPay = 'QuickPayPC';
+// const assets = [
+//   '/',
+//   '/index.html',
+//   '/biweeklypay.html',
+//   '/hourlypayfrequency.html',
+//   '/salarybiweekly.html',
+//   '/salarymonthly.html',
+//   '/salarypayfrequency.html',
+//   '/css/style.css',
+//   '/js/app.js',
+//   '/img/QuickPay-Logo.png'
+// ];
+
+self.addEventListener('install', (installEvent) => installEvent);
